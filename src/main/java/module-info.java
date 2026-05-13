01840b3409750d19069c00aef491e616ff70417f
des-albert <des.albert@gmail.com>
2026-05-13 07:02:33 -0700

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,6 @@ module org.dba.xdtest {
     requires org.apache.poi.ooxml;
     requires kotlinx.coroutines.core;
     requires kotlinx.coroutines.javafx;
-    requires java.prefs;
 
 
     opens org.dba.xdtest to javafx.fxml;
